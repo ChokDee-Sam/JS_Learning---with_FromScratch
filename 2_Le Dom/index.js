885528af5455b5cee6ee67c22a6ba3db1f221fbc
ChokDee-Sam <chokdee.sam@hotmail.com>
2022-10-10 23:59:06 -1000

    Commit 3 - Mouse Event + Exercice

--- 2_Le Dom/index.js
+++ 2_Le Dom/index.js
@@ -1,6 +1,6 @@
-// ***************
+// *********************************************
 // SELECTEURS
-// ***************
+// *********************************************
 
 // selectionner
 // document.querySelector("p");
@@ -14,33 +14,62 @@
 // Utiliser cette variable qui selectionne pour changer
 // reponse.style.visibility = "visible";
 
-// ***************
+// *********************************************
 // CLICK EVENT
-// ***************
+// *********************************************
 
 // création d'une fonction de bonne réponse dans Console + visible
 function bonneReponseFunction() {
     console.log("Bonne réponse");
-    document.querySelector("p ").style.visibility = "visible";
-    document.querySelector("#btn-3").style.backgroundColor = "white";
-    document.querySelector("#btn-3").style.boxShadow = "-1px 1px 20px 12px rgb(255 255 255 / 26%)";
-    document.querySelector("#btn-3").style.color = "teal";
-    document.querySelector("#btn-3").style.padding = "10px 30px";
-    document.querySelector("#btn-3").style.transform = "scale(1.60) rotate(-10deg)  translate(-90px, -40px)";
-    document.querySelector("#btn-1").style.transform = "scale(.90)translate(0px, 50px)";
-    document.querySelector("#btn-1").style.opacity = "0";
-    document.querySelector("#btn-2").style.transform = "scale(.90)translate(0px, 50px)";
-    document.querySelector("#btn-2").style.opacity = "0";
+    // document.querySelector("p").style.visibility = "visible";
+    // on peut rajouter du style, mais une class est mieux pour plusieurs style
+    document.querySelector("p").classList.toggle("p-visible");
 
+    // on ajoute une class css plutot que d'écrire plusieurs lignes + toggle
+    document.querySelector("#btn-1").classList.toggle("mauvaises-reponses");
+    document.querySelector("#btn-2").classList.toggle("mauvaises-reponses");
+    document.querySelector("#btn-3").classList.toggle("bonne-reponse");
 }
 
-// Créer une variable qui selectionne la zone .click-event
-const bonneReponse = document.querySelector("#btn-3");
+// Créer une variable qui selectionne la zone
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.querySelector("#btn-2");
+const btn3 = document.querySelector("#btn-3");
 
-// Variable à l'evenement du click qui déclenche une fonction flechée
-bonneReponse.addEventListener("click", () => {
-    console.log("Hey Hey Hey !!");
+// Var Définie la zone + Evenement + Fonction flêchée
+btn1.addEventListener("click", () => {
+    btn1.classList.toggle("faux");
+});
+btn2.addEventListener("click", () => {
+    btn2.classList.toggle("faux");
 });
 
-// Variable à l'evenement du click qui declenche une fonction créée en amont
-bonneReponse.addEventListener("click", bonneReponseFunction);
+// Var Définie la zone + Evenement + Fonction (préalablement déclarée)
+// Où               Quoi       Event         Comment
+btn3.addEventListener("click", bonneReponseFunction);
+
+// *********************************************
+// MOUSE EVENT
+// *********************************************
+
+const mouseMove = document.querySelector(".mouse-move");
+
+// Prend les coordonnées X et Y pour mettre cette info dans style
+window.addEventListener("mousemove", (e) => {
+    mouseMove.style.left = e.pageX + "px";
+    mouseMove.style.top = e.pageY + "px";
+});
+
+// Event au click enfoncé
+window.addEventListener("mousedown", () => {
+    mouseMove.style.transform = "scale(2) translate(-25%, -25%";
+    mouseMove.style.borderColor = "white";
+});
+
+// Event au click relaché
+window.addEventListener("mouseup", () => {
+    mouseMove.style.transform = "scale(1) translate(-50%, -50%)";
+    mouseMove.style.borderColor = "teal";
+});
+
+// Events autres : mouseenter / mouseout / mouseover
