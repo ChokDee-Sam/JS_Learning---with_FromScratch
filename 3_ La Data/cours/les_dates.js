a62d7ac58e371cba00763d66c47b11857c89679c
ChokDee-Sam <chokdee.sam@hotmail.com>
2023-01-28 19:27:22 -1000

    Commit 23 - Data : Le Destructuring

--- 3_ La Data/cours/les_dates.js
+++ 3_ La Data/cours/les_dates.js
@@ -65,3 +65,57 @@ console.log(`--- Function dateParser()`);
 console.log(dateParser(date));
 console.log(dateParser(iso));
 console.log(dateParser(timestamp));
+
+// -------------------------------------
+// LE DESTRUCTURING (permet d'écrire plus court)
+// -------------------------------------
+
+// Création d'un objet contenant un  tableau
+let moreData = {
+    destVar: ["Element 1", "Element 2"],
+};
+// -----------
+
+// Création d'une Const en destructuring :
+const { destVar } = moreData;
+
+// C'est la même chose qu'écrire :
+const destVar2 = moreData.destVar;
+
+// Exemple
+console.log(moreData.destVar);
+console.log(destVar);
+
+// -----------
+
+let arrayExemple = [10, 20, 30];
+console.log(arrayExemple[0]);
+console.log(arrayExemple[1]);
+console.log(arrayExemple[2]);
+
+// On déstructure individuellement le contenu d'un tableau dans une variable
+let [x, y, z] = arrayExemple;
+console.log(x);
+console.log(y);
+console.log(z);
+
+// -----------
+
+// Exemple en mixant une date + le destructuring
+
+console.log(iso);
+const dateDestructuring = (chaine) => {
+    // La chaine se coupe au T et qu'on garde uniquement la premiere partie
+    let newDate = chaine.split("T")[0];
+    console.log(newDate);
+
+    newDate = newDate.split("-"); // on 'découpe' en plusieurs elements de tableau
+    console.log(newDate);
+
+    let [y, m, d] = newDate; // on crée une variable pour chaque element de tableau
+    console.log(d, m, y);
+    
+    let newDateFR = [d, m, y] // on crée une variable contenant un tableau de var dans le bon ordre
+    return newDateFR.join(' / ') // et on les join
+};
+console.log(dateDestructuring(iso)); // appel de fonction avec iso en argument
