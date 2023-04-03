4a963de7b6089cf36d7d9b6c3d69f52cd106131b
ChokDee Sam <chokdee.sam@hotmail.com>
2023-04-02 23:54:18 -1200

    Commit 33 - API : Projet User App [Part 1.2]

--- 4_Les API/3. Async/API users App/index.js
+++ 4_Les API/3. Async/API users App/index.js
@@ -5,27 +5,31 @@
 // ----------------------------------------------------------
 // ----------------------------------------------------------
 
-let userData = [];
+let userData;
 
 const fetchUser = async () => {
     await fetch("https://randomuser.me/api/?results=24")
         .then((res) => res.json())
         .then((data) => (userData = data.results));
-    console.log(userData);
+    console.log(userData); //affiche correctement le contenu
+    console.log(userData.length);
 };
 
-// fetchUser();
-
+// fetchUser()
+console.log(userData); // undefined
 // ----------------------------------------------------------
 
 const userDisplay = async () => {
     await fetchUser();
 
-    document.body.innerHTML = userData.map(
-        (user) =>
-            `
-        <h3>${user.name.first} </h3>
-        `
-    );
+    // Avec .map()
+    document.body.innerHTML = userData
+        .map(
+            (user) =>
+                `
+                <h3>${user.name.first} </h3>
+                `
+        )
+        .join("");
 };
 userDisplay();
