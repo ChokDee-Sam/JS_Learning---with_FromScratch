b97c4629b61d0c41a41f03ce9e404f8afc204790
ChokDee Sam <chokdee.sam@hotmail.com>
2023-04-03 20:58:59 +0700

    Commit 36 - API : Projet User App [Part 2]

--- 4_Les API/3. Async/API users App/index.js
+++ 4_Les API/3. Async/API users App/index.js
@@ -13,6 +13,7 @@ const fetchUser = async () => {
         .then((data) => (userData = data.results));
     console.log(userData); //affiche correctement le contenu
     console.log(userData.length);
+    console.log(userData[0]);
 };
 
 // fetchUser()
@@ -22,24 +23,49 @@ console.log(userData); // undefined
 const userDisplay = async () => {
     await fetchUser();
 
+    const dateParser = (date) => {
+        let newDate = new Date(date).toLocaleDateString("fr-TH", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+        });
+        return newDate;
+    };
+
     // Avec .map()
     document.body.innerHTML = userData
         .map(
             (user) =>
                 `
+                <div class="card">
+                <img src=${user.picture.large}
+                        alt="photo de ${user.name.first}">
                 <h3>${user.name.first} </h3>
+                <p>${user.location.city}, ${dateParser(user.dob.date)} </p>
+
+                <em> 
+                Membre depuis : ${
+                    user.registered.date
+                }jours</em>                
+                </div>
                 `
         )
         .join("");
 
-    // avec Boucle For
-    for (let i = 0; i < userData.length; i++) {
-        document.body.innerHTML += `<h3> ${userData[i].location.city}</h3>`;
-    }
+    // // avec Boucle For
+    // for (let i = 0; i < userData.length; i++) {
+    //     document.body.innerHTML += `<h3> ${userData[i].location.city}</h3>`;
+    // }
 
-    // avec Boucle For Of
-    for (const element of userData) {
-        document.body.innerHTML += `<h3> ${element.gender}</h3>`;
-    }
+    // // avec Boucle For Of
+    // for (const element of userData) {
+    //     document.body.innerHTML += `<h3> ${element.gender}</h3>`;
+    // }
+
+    // // avec Boucle For Each
+    // userData.forEach(element => {
+    //     document.body.innerHTML += `<h3> ${element.dob.age}</h3>`
+
+    // });
 };
 userDisplay();
