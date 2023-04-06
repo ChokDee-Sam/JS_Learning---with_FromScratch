3e2dfbcb8e9737d592603f60428e17697238a098
ChokDee-Sam <chokdee.sam@hotmail.com>
2023-04-05 22:32:19 -1000

    Commit 45 - POO : Partie cours [Quelques Méthodes utiles]

--- 5_ La POO/script.js
+++ 5_ La POO/script.js
@@ -43,3 +43,38 @@ for (const abc in obj) {
 for (const abc in obj) {
     console.log(abc + " : " + obj[abc]); // affiche les valeurs de chaque propriété
 }
+
+// -------------------------------------------------------------------
+// Quelques Méthodes utiles
+// -------------------------------------------------------------------
+
+// Obtenir les clés
+const obtenirLesClefs = Object.keys(obj);
+console.log(obtenirLesClefs);
+
+// Obtenir les valeurs
+const obtenirLesValeurs = Object.values(obj);
+console.log(obtenirLesValeurs);
+
+// Décompose l'objet avec chaque propriété/valeur dans un tableau
+const nestedArray = Object.entries(obj);
+console.log(nestedArray);
+
+// Fusionner objects
+// S'il y a 2 mêmes propriétés, c'est la dernière qui écrase
+const obj2 = {
+    taille: "grand",
+};
+const fusionner = Object.assign({}, obj, obj2);
+console.log(fusionner);
+
+// Empêcher les modifications ou ajout
+const newObj = Object.freeze(obj);
+newObj.nom = "SamSam"; // cette modification ne fonctionnera pas
+newObj.hobby = "Moto"; // cet ajout ne fonctionnera pas
+console.log(newObj);
+
+const newObj2 = Object.seal(obj2);
+newObj2.taille = "Très grand"; // modification OK
+newObj2.hobby = "Moto"; // cet ajout ne fonctionnera pas
+console.log(newObj2);
